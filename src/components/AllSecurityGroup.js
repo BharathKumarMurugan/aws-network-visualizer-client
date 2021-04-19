bd679429ea8f6268554e5d6ac8f4aa6c610227e1
Bharath Kumar <bharathkumarmurugan2016@gmail.com>
2021-04-19 22:40:32 +0530

    Update AllSecurityGroup.js

--- src/components/AllSecurityGroup.js
+++ src/components/AllSecurityGroup.js
@@ -1,46 +1,35 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Loader from "./Loader";
+function AllSecurityGroups() {
+    useEffect(() => {
+        fetchItems();
+    }, []);
 
-class AllSecurityGroup extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            allSecurityGroups: [],
-            isLoading: false,
-            isError: false,
-        };
-    }
-    async componentDidMount() {
-        this.setState({ isLoading: true });
-        const response = await fetch("http://localhost:5000/api/networks/");
+    const [items, setItems] = useState([]);
+    const [isLoading, setIsLoading, isError, setIsError] = useState(false);
+
+    const fetchItems = async () => {
+        setIsLoading(true);
+        const response = await fetch("");
         if (response.ok) {
-            const allSecurityGroups = response.json();
-            this.setState({ allSecurityGroups, isLoading: false });
+            const items = await response.json();
+            console.log(items);
+            setItems(items);
+            setIsLoading(false);
         } else {
-            this.setState({ isLoading: false, isError: true });
+            setIsLoading(false);
+            setIsError(true);
         }
-    }
-    renderTableHeader = () => {
-        return Object.v;
     };
-    renderTableRow = () => {};
-    render() {
-        const { allSecurityGroups, isLoading, isError } = this.state;
-        if (isLoading) return <Loader />;
-        if (isError) return <div>Error...</div>;
-        return allSecurityGroups.length > 0 ? (
-            <div className="table-responsive">
-                <table className="table table-hover">
-                    <thead className="thead-dark">
-                        <tr>{this.renderTableHeader()}</tr>
-                    </thead>
-                    <tbody>{this.renderTableRow()}</tbody>
-                </table>
-            </div>
-        ) : (
-            <div>0 Security Groups</div>
-        );
-    }
+    if (isLoading) return <Loader />;
+    if (isError) return <div>Error</div>;
+    return items.length > 0 ? (
+        <div>
+            <h2>SecurityGroups</h2>
+        </div>
+    ) : (
+        <div>0 SecurityGroups</div>
+    );
 }
 
-export default AllSecurityGroup;
+export default AllSecurityGroups;
