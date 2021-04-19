62070b5688355318268c612085f69f0ccafbb15a
Bharath Kumar <bharathkumarmurugan2016@gmail.com>
2021-04-19 12:41:09 +0530

    Update AllItem.js

--- src/components/AllItem.js
+++ src/components/AllItem.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Loader from "./Loader";
 
-function AllItems() {
-    return (
+function AllItems({ match }) {
+    useEffect(() => {
+        fetchItems();
+    }, []);
+
+    const [items, setItems] = useState([]);
+    const [isLoading, setIsLoading, isError, setIsError] = useState(false);
+
+    const fetchItems = async () => {
+        setIsLoading(false);
+        const response = await fetch("");
+        if (response.ok) {
+            const items = await response.json();
+            console.log(items);
+            setItems(items);
+            setIsLoading(false);
+        } else {
+            setIsError(true);
+            setIsLoading(false);
+        }
+    };
+    if (isLoading) return <Loader />;
+    if (isError) return <div>Error...</div>;
+    return items.length > 0 ? (
         <div>
             <h1>All Items page</h1>
         </div>
+    ) : (
+        <div>0 Items</div>
     );
 }
 
