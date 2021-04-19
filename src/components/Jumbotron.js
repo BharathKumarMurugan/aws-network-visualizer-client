a1b34d4c393f1e345b474254ad9ce88de615c08d
Bharath Kumar <bharathkumarmurugan2016@gmail.com>
2021-04-19 12:41:42 +0530

    Update Jumbotron.js

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -2,14 +2,16 @@ import React from "react";
 
 function Jumbotron() {
     return (
-        <div class="p-5 mb-4 bg-light">
-            <div class="container-fluid py-5">
-                <h1 class="display-5 fw-bold">AWS Infrastructure Visualizer</h1>
-                <div class="clearfix">
-                    <p class="float-start">Account Name</p>
-                    <p class="float-end">Region Name</p>
+        <div className="p-5 mb-4 bg-light">
+            <div className="container-fluid py-5">
+                <h1 className="display-5 fw-bold">
+                    AWS Infrastructure Visualizer
+                </h1>
+                <div className="clearfix">
+                    <p className="float-start">Account Name</p>
+                    <p className="float-end">Region Name</p>
                 </div>
-                <p class="col-md-8 fs-4">
+                <p className="col-md-8 fs-4">
                     A Tool to visualize Amazon Cloud Infrastructure.
                 </p>
             </div>
