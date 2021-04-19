039a96fa646eb1771005eaecf1a2d7d13264f4ea
Bharath Kumar <bharathkumarmurugan2016@gmail.com>
2021-04-19 12:41:25 +0530

    Update Loader.js

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./table.css";
 
-const Loader = () => {
+function Loader() {
     return (
-        <div class="spinner-border text-primary" role="status">
-            <span class="sr-only">Loading...</span>
-        </div>
-        // <div id="wrapper">
-        //     <div className="loading-spinner"></div>
+        // <div className="spinner-border text-primary" role="status">
+        //     <span className="sr-only">Loading...</span>
         // </div>
+        <div id="wrapper">
+            <div className="loading-spinner"></div>
+        </div>
     );
-};
+}
 export default Loader;
