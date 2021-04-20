fd1ec743d68c2acfb902812748781c63d1046134
Bharath Kumar <bharathkumarmurugan2016@gmail.com>
2021-04-20 22:49:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import VPC from "./components/VPC";
 import "./App.css";
+import AllVPC from "./components/AllVPC";
 import NavBar from "./components/NavBar";
 import Jumbotron from "./components/Jumbotron";
 import AllItems from "./components/AllItem";
@@ -13,7 +13,7 @@ function App() {
                 <NavBar />
                 <Switch>
                     <Route path="/" exact component={Jumbotron} />
-                    <Route path="/vpc" exact component={VPC} />
+                    <Route path="/vpc" exact component={AllVPC} />
                     <Route path="/vpc/:id" component={AllItems} />
                 </Switch>
             </div>
