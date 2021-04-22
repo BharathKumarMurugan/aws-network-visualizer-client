e0682b2171fcfad1b6ada1309822eacccafb1a33
Bharath Kumar <bharathkumarmurugan2016@gmail.com>
2021-04-22 12:13:52 +0530

    Update InternetGateway.js

--- src/components/InternetGateway.js
+++ src/components/InternetGateway.js
@@ -1,18 +1,47 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-function InternetGateway() {
-    const renderTableHeader = () => {};
-    const renderTableRow = () => {};
+function InternetGateway({ items }) {
+    const renderTableHeader = () => {
+        return Object.keys(items[0]).map((attr) => <th key={attr}>{attr}</th>);
+    };
+    const renderTableRow = () => {
+        return items.map((igw) => {
+            return (
+                <tr key={igw.Id}>
+                    <td>{igw.Id}</td>
+                    <td>{igw.Name}</td>
+                    <td>{igw.State}</td>
+                </tr>
+            );
+        });
+    };
 
-    return (
-        <div className="table-reponsive">
-            <table className="table table-hover">
-                <thead className="thead-dark">
-                    <tr>{renderTableHeader()}</tr>
-                </thead>
-                <tbody>{renderTableRow()}</tbody>
-            </table>
+    return items.length > 0 ? (
+        <div className="card border-light shadow-sm p-2 mb-5 bg-body rounded">
+            <div className="card-body">
+                <h5 className="card-title">
+                    Internet Gateway{" "}
+                    <span className="badge rounded-pill bg-primary">
+                        {items.length}
+                    </span>
+                </h5>
+                <div className="table-reponsive">
+                    <table className="table table-hover">
+                        <thead className="thead-dark">
+                            <tr>{renderTableHeader()}</tr>
+                        </thead>
+                        <tbody>{renderTableRow()}</tbody>
+                    </table>
+                </div>
+            </div>
         </div>
+    ) : (
+        <h5 className="card-title">
+            Internet Gateway{" "}
+            <span className="badge rounded-pill bg-primary">
+                {items.length}
+            </span>
+        </h5>
     );
 }
 
