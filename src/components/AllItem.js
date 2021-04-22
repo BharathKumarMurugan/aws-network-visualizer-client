6aa62742d1be1f8b47ade500537d84fbedbc0917
Bharath Kumar <bharathkumarmurugan2016@gmail.com>
2021-04-22 12:12:24 +0530

    Update AllItem.js

--- src/components/AllItem.js
+++ src/components/AllItem.js
@@ -15,13 +15,12 @@ function AllItems({ match }) {
     const [isLoading, setIsLoading, isError, setIsError] = useState(false);
 
     const fetchItems = async () => {
-        setIsLoading(false);
+        setIsLoading(true);
         const response = await fetch(
             `http://localhost:5000/api/networks/vpc/?vpcId=${match.params.id}`
         );
         if (response.ok) {
             const items = await response.json();
-            console.log(items);
             setItems(items);
             setIsLoading(false);
         } else {
@@ -33,7 +32,10 @@ function AllItems({ match }) {
     if (isError) return <div>Error...</div>;
     return items.length > 0 ? (
         <div>
-            <h1>All Items page</h1>
+            <Vpc items={items[0]} />
+            <Subnet items={items[1]} />
+            <InternetGateway items={items[2]} />
+            <AllSecurityGroups items={items[3]} />
         </div>
     ) : (
         <div>0 Items</div>
