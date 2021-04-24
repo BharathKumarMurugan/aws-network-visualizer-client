6ecb598839ffa9fc92dc5b93f46389e9d8cdfd62
Bharath Kumar <bharathkumarmurugan2016@gmail.com>
2021-04-24 23:55:16 +0530

    Update RouteTable.js

--- src/components/RouteTable.js
+++ src/components/RouteTable.js
@@ -1,59 +1,67 @@
-import React, { useEffect, useState } from "react";
-import Loader from "./Loader";
+import React from "react";
 
-function RouteTable() {
-    useEffect(() => {
-        fetchItems();
-    }, []);
-
-    const [items, setItems] = useState([]);
-    const [isLoading, setIsLoading, isError, setIsError] = useState(false);
-
-    const fetchItems = async () => {
-        setIsLoading(true);
-        const response = await fetch("");
-        if (response.ok) {
-            const items = await response.json();
-            console.log(items);
-            setItems(items);
-            setIsLoading(false);
-        } else {
-            setIsLoading(false);
-            setIsError(true);
-        }
-    };
+function RouteTable({ items }) {
     const renderTableHeader = () => {
         return Object.keys(items[0]).map((attr) => <th key={attr}>{attr}</th>);
+    };
+    const arrayFormater = (input) => {
+        if (Array.isArray(input)) {
+            return input.map((i) =>
+                i === "" || null ? (
+                    ""
+                ) : (
+                    <span>
+                        {i}
+                        <br />
+                    </span>
+                )
+            );
+        }
     };
     const renderTableRow = () => {
         return items.map((rt) => {
             return (
                 <tr key={rt.Id}>
                     <td>{rt.Id}</td>
-                    <td>{rt.Id}</td>
-                    <td>{rt.Id}</td>
-                    <td>{rt.Id}</td>
-                    <td>{rt.Id}</td>
-                    <td>{rt.Id}</td>
-                    <td>{rt.Id}</td>
+                    <td>{rt.Name}</td>
+                    <td>{arrayFormater(rt.SubnetAssociation)}</td>
+                    <td>{rt.Main}</td>
+                    <td>{arrayFormater(rt.RoutesDestination)}</td>
+                    <td>{arrayFormater(rt.RoutesTarget)}</td>
+                    <td>{arrayFormater(rt.RoutesState)}</td>
                 </tr>
             );
         });
     };
 
-    if (isLoading) return <Loader />;
-    if (isError) return <div>Error...</div>;
     return items.length > 0 ? (
-        <div className="table-reponsive">
-            <table className="table table-hover">
-                <thead className="thead-dark">
-                    <tr>{renderTableHeader()}</tr>
-                </thead>
-                <tbody>{renderTableRow()}</tbody>
-            </table>
+        <div className="card border-light shadow-sm p-2 mb-5 bg-body rounded">
+            <div className="card-body">
+                <h5 className="card-title">
+                    Route Table{" "}
+                    <span className="badge rounded-pill bg-primary">
+                        {items.length}
+                    </span>
+                </h5>
+                <div className="table-reponsive">
+                    <table className="table table-hover">
+                        <thead className="thead-dark">
+                            <tr>{renderTableHeader()}</tr>
+                        </thead>
+                        <tbody>{renderTableRow()}</tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     ) : (
-        <div>0 Route Tables</div>
+        <div className="card border-light shadow-sm p-2 mb-5 bg-body rounded">
+            <h5 className="card-title">
+                Route Table{" "}
+                <span className="badge rounded-pill bg-primary">
+                    {items.length}
+                </span>
+            </h5>
+        </div>
     );
 }
 
